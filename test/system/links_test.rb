ee9f8f63461d77e38a21c70c45ace56fbc6f25eb
Juan Ibiapina <juanibiapina@gmail.com>
2025-06-05 18:06:34 +0200

    test: Fix system tests

--- test/system/links_test.rb
+++ test/system/links_test.rb
@@ -3,40 +3,41 @@ require "application_system_test_case"
 class LinksTest < ApplicationSystemTestCase
   setup do
     @link = links(:one)
+    @trip = trips(:one)
+    @user = users(:one)
+    sign_in @user
   end
 
   test "visiting the index" do
-    visit links_url
+    visit trip_links_url(@trip)
     assert_selector "h1", text: "Links"
   end
 
   test "should create link" do
-    visit links_url
+    visit trip_links_url(@trip)
     click_on "New link"
 
-    fill_in "Trip", with: @link.trip_id
     fill_in "Url", with: @link.url
     click_on "Create Link"
 
     assert_text "Link was successfully created"
-    click_on "Back"
+    click_on "Back to trip"
   end
 
   test "should update Link" do
-    visit link_url(@link)
+    visit trip_link_url(@trip, @link)
     click_on "Edit this link", match: :first
 
-    fill_in "Trip", with: @link.trip_id
     fill_in "Url", with: @link.url
     click_on "Update Link"
 
     assert_text "Link was successfully updated"
-    click_on "Back"
+    click_on "Back to trip"
   end
 
   test "should destroy Link" do
-    visit link_url(@link)
-    accept_confirm { click_on "Destroy this link", match: :first }
+    visit trip_link_url(@trip, @link)
+    click_on "Destroy this link", match: :first
 
     assert_text "Link was successfully destroyed"
   end
