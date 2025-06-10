b3d089223b4f0aecf33d52ec89005da1f997ede5
Juan Ibiapina <juanibiapina@gmail.com>
2025-06-10 21:18:37 +0200

    Update sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,21 +1,6 @@
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
-
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for tracing.
-  # We recommend adjusting this value in production
-  config.traces_sample_rate = Rails.env.production? ? 0.1 : 1.0
-  # or
-  # config.traces_sampler = lambda do |context|
-  #   true
-  # end
-
-  # Set profiles_sample_rate to profile 100%
-  # of sampled transactions.
-  # We recommend adjusting this value in production.
-  config.profiles_sample_rate = Rails.env.production? ? 0.1 : 1.0
-
-  config.environment = Rails.env
-  config.enabled_environments = %w[ production staging ]
+  config.send_default_pii = true
+  config.environment = ENV["ENVIRONMENT"]
 end
